2ee0c51b29e3f7194d8f305b97efb555664dc357
nari19 <kazuki7793996@gmail.com>
2018-06-29 14:10:21 +0900

    file rename

--- write.py
+++ write.py
@@ -1,17 +1,24 @@
-# svgwriteで生成
+# 縦は白銀比 1:1.41
+# 横は黄金比 1:1.61         tate yoko shikaku
+# https://flatuicolors.com/palette/us インデックスカラー
 
-# viewBox="-21.5 -21.5 100 100"
-import svgwrite
 
-# dwg = svgwrite.Drawing('Circle.svg', profile='tiny')
-dwg = svgwrite.Drawing('react.svg')
-marker = dwg.marker(insert=(5,5), size=(10,10))
+for i in ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#dfe6e9", "#00b894", "#00cec9", "#0984e3", "#6c5ce7", "#b2bec3", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8", "#636e72", "#fdcb6e", "#e17055", "#d63031", "#e84393", "#2d3436"]:
+    # --------------------
+    file_name = "images/" + i + ".svg"
+    ratio = "tate" 
+    # --------------------
+    f = open(file_name, 'w', encoding='utf-8')
+    if ratio == "tate":
+        c0 = '<?xml version="1.0" encoding="utf-8" ?><svg xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" baseProfile="full" version="1.1" viewBox="0 0 200 280"><rect width="100%" height="100%" fill="aaaaa"/></svg>'
+    elif ratio == "yoko":
+        c0 = '<?xml version="1.0" encoding="utf-8" ?><svg xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" baseProfile="full" version="1.1" viewBox="0 0 360 200"><rect width="100%" height="100%" fill="aaaaa"/></svg>'
+    else:
+        c0 = '<?xml version="1.0" encoding="utf-8" ?><svg xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" baseProfile="full" version="1.1" viewBox="0 0 300 300"><rect width="100%" height="100%" fill="aaaaa"/></svg>'
+    source = c0.replace("aaaaa", i)
 
-marker.add(dwg.circle((5, 5), r=5, fill='red'))
-dwg.defs.add(marker)
-line = dwg.add(dwg.polyline(
-[(10, 10), (50, 20), (70, 50), (100, 30)],
-stroke='black', fill='none'))
-line.set_markers(marker)
-line['marker'] = marker.get_funciri()
-dwg.save()
+    print (i)
+
+    f.write(source)
+    f.flush()
+    f.close()
