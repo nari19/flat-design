9410ec0173bf7c6c0ca4eebf60aab30cf9fea2a6
nari19 <kazuki7793996@gmail.com>
2018-06-29 14:37:24 +0900

    comment edit

--- write.py
+++ write.py
@@ -1,7 +1,3 @@
-# 縦は白銀比 1:1.41
-# 横は黄金比 1:1.61         tate yoko shikaku
-# https://flatuicolors.com/palette/us インデックスカラー
-
 
 for i in ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#dfe6e9", "#00b894", "#00cec9", "#0984e3", "#6c5ce7", "#b2bec3", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8", "#636e72", "#fdcb6e", "#e17055", "#d63031", "#e84393", "#2d3436"]:
     # --------------------
