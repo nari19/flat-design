1f4b48d3cd4c78e5ec59cdd9f948a23bb4c5a088
nari19 <kazuki7793996@gmail.com>
2018-07-09 14:05:48 +0900

    funcs

--- funcs.py
+++ funcs.py
@@ -1,18 +1,40 @@
 from main import t
 from write import wr
+
+
 def t_edit(t):
+    cn = {
+        c1: "#55efc4", c2: "#81ecec", c3: "#74b9ff", c4: "#a29bfe", c5: "#dfe6e9", 
+        c6: "#00b894", c7: "#00cec9", c8: "#0984e3", c9: "#6c5ce7", c10: "#b2bec3",
+        c11: "#ffeaa7", c12: "#fab1a0", c13: "#ff7675", c14: "#fd79a8", c15: "#636e72", 
+        c16: "#fdcb6e", c17: "#e17055", c18: "#d63031", c19: "#e84393", c20: "#2d3436"
+    }
+    fn = {
+        f1: "Menlo", f2: "Monaco", f3: "M+ 1c light", f4: "M+ 1plight", f5: "Myra 4F Caps Bold"
+    }
+    sn = {
+        s1: "30", s2: "" 
+    }
+    key = [u for u, x in enumerate(t) if x == "y"]
+    for i in t[]:
+
+
+
+    wr(t)
+# a = '5'
+# d = {a: int(a)*30}
+# print(d[a])
     # (tのインデント) = (アルファベットと数字の2文字を抽出する正規表現)
     # for (条件:)
     # t[] = t[] * 200 / 10
     # pixel
-    p1 = x * 200 / 10
-    # color
-    c1 = "#55efc4"
-    c2 = "#81ecec"
-    c3 = "#74b9ff"
+    # p1 = x * 200 / 10
+    # # color
+    # c1 = "#55efc4"
+    # c2 = "#81ecec"
+    # c3 = "#74b9ff"
     # font
 
-    wr(t)
 # 配色インデックスカラー
 # px比率
 # 例外処理
